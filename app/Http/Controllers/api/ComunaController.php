43404c9683ab159f25d9965b24624b6c25b32257
Oscar Eduardo Jaramillo Nuñez <38515273@claro.com.co>
2025-05-19 08:53:15 -0500

    Implemente los métodos en ComunaController

--- app/Http/Controllers/api/ComunaController.php
+++ app/Http/Controllers/api/ComunaController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Comuna;
+use App\Models\Municipio;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 
 class ComunaController extends Controller
 {
@@ -12,7 +16,12 @@ class ComunaController extends Controller
      */
     public function index()
     {
-        //
+        $comunas = DB::table('tb_comuna')
+            ->join('tb_municipio', 'tb_comuna.muni_codi', '=', 'tb_municipio.muni_codi')
+            ->select('tb_comuna.*', 'tb_municipio.muni_nomb')
+            ->get();
+            
+        return json_encode(['comunas' => $comunas]);
     }
 
     /**
@@ -20,30 +29,85 @@ class ComunaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validate = Validator::make($request->all(), [
+            'comu_nomb' => ['required', 'max:30', 'unique'],
+            'muni_codi' => ['required', 'numeric', 'min:1']
+        ]);
+
+        if ($validate->fails()) {
+            return response()->json([
+                'msg' => 'Se produjo un error en la validación de la información.',
+                'statusCode' => 400
+            ]);
+        }
+
+        $comuna = new Comuna();
+        $comuna->comu_nomb = $request->comu_nomb;
+        $comuna->muni_codi = $request->muni_codi;
+        $comuna->save();
+        
+        return json_encode(['comuna' => $comuna]);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show($id)
     {
-        //
+        $comuna = Comuna::find($id);
+        if (is_null($comuna)) {
+            return abort(404);
+        }
+        
+        $municipios = DB::table('tb_municipio')
+            ->orderBy('muni_nomb')
+            ->get();
+            
+        return json_encode(['comuna' => $comuna, 'municipios' => $municipios]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, $id)
     {
-        //
+        $validate = Validator::make($request->all(), [
+            'comu_nomb' => ['required', 'max:30', 'unique'],
+            'muni_codi' => ['required', 'numeric', 'min:1']
+        ]);
+
+        if ($validate->fails()) {
+            return response()->json([
+                'msg' => 'Se produjo un error en la validación de la información.',
+                'statusCode' => 400
+            ]);
+        }
+
+        $comuna = Comuna::find($id);
+        $comuna->comu_nomb = $request->comu_nomb;
+        $comuna->muni_codi = $request->muni_codi;
+        $comuna->save();
+        
+        return json_encode(['comuna' => $comuna]);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy($id)
     {
-        //
+        $comuna = Comuna::find($id);
+        if (is_null($comuna)) {
+            return abort(404);
+        }
+        
+        $comuna->delete();
+        
+        $comunas = DB::table('tb_comuna')
+            ->join('tb_municipio', 'tb_comuna.muni_codi', '=', 'tb_municipio.muni_codi')
+            ->select('tb_comuna.*', 'tb_municipio.muni_nomb')
+            ->get();
+            
+        return json_encode(['comunas' => $comunas, 'success' => true]);
     }
 }
